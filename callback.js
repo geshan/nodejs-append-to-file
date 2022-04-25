9592976ea8c4526233e64db4c7804fa611bb7a91
Geshan Manandhar <geshan@simplywallst.com>
2022-04-25 21:28:22 +1000

    Formats the callback example with 2 spaces

--- callback.js
+++ callback.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
 fs.appendFile('file.txt', 'First line added!', 'utf-8', err => {
+  if (err) {
+    throw err;
+  }
+  console.log('First line written.');
+  fs.appendFile('file.txt', '\nSecond line appended.', err => {
     if (err) {
-        throw err;
+      throw err;
     }
-    console.log('First line written.');
-    fs.appendFile('file.txt', '\nSecond line appended.', err => {
-      if (err) {
-        throw err;
-      }
 
-      console.log('Second line appended.');
-    });
+    console.log('Second line appended.');
+  });
 });
