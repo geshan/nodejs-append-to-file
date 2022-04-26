6393853fa06eb2897f1cd82120aa1f096d03242e
Geshan Manandhar <geshan@simplywallst.com>
2022-04-26 21:13:08 +1000

    Adds await to the async appendFile calls

--- promise.js
+++ promise.js
@@ -3,8 +3,8 @@ const fsPromises = require('fs/promises');
 (async () => {
   try {
     const fileName = 'file-promise.txt'
-    fsPromises.appendFile(fileName, 'First line added!', 'utf-8');
-    fsPromises.appendFile(fileName, '\nSecond line appended.', 'utf-8');
+    await fsPromises.appendFile(fileName, 'First line added!', 'utf-8');
+    await fsPromises.appendFile(fileName, '\nSecond line appended.', 'utf-8');
     console.log('Added 2 lines to file.');
   } catch(err) {
     console.log('Error appending data to file', err);
